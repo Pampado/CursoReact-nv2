1db092954fac1ffcfdec2dd5fe18fff4ada8b242
pampado <pampado@hotmail.com>
2020-04-23 00:53:47 -0400

    revisao merge

--- desafio6/src/app.ts
+++ desafio6/src/app.ts
@@ -1,9 +1,6 @@
 import 'reflect-metadata'
 import 'dotenv/config'
-<<<<<<< HEAD
 import cors from 'cors'
-=======
->>>>>>> 14a38b9d68e6616c16f66da9ada3f5126f03cd6e
 
 import express, { Request, Response, NextFunction } from 'express'
 import 'express-async-errors'
@@ -16,10 +13,7 @@ import createConnection from './database'
 createConnection()
 const app = express()
 
-<<<<<<< HEAD
 app.use(cors())
-=======
->>>>>>> 14a38b9d68e6616c16f66da9ada3f5126f03cd6e
 app.use(express.json())
 app.use(routes)
 
@@ -31,11 +25,7 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
     })
   }
 
-<<<<<<< HEAD
   // console.error(err)
-=======
-  console.error(err)
->>>>>>> 14a38b9d68e6616c16f66da9ada3f5126f03cd6e
 
   return response.status(500).json({
     status: 'error',
